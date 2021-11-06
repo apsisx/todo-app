36ba712249a86fe02b965f8e7506a50433910365
Muhammed ARSLAN <46113819+apsisx@users.noreply.github.com>
2021-11-07 01:20:14 +0300

    Everything finished
    -- Add, Update, Delete functions are written.
    -- Edit bottom bar in UI is fixed
    -- Unnecessary column is removed from DB
    -- Generated APIs for communications of CRUD functions
    -- TODO: Comment sections will written

--- routes/api.js
+++ routes/api.js
@@ -4,8 +4,26 @@ var path = require('path');
 
 var queries = require('../models/queries')
 
-router.get('/getlist', async (req, res) => {
+router.get('/todo', async (req, res) => {
     const list = await queries.getList()
     res.json(list)
+})
+
+router.post('/todo', async (req, res) => {
+    const addToDoText = await queries.addToDoList(req.body)
+    const list = await queries.getList()
+    res.json(list)
+})
+
+router.post('/todo-control', async (req, res) => {
+    if (req.body.command == 'delete') {
+        const deleteTodoItem = await queries.deleteTodoItem(req.body.id)
+        const list = await queries.getList()
+        res.json(list)
+    } else if (req.body.command == 'update') {
+        const editToDoItem = await queries.editToDoItem(req.body.id, {t_name: req.body.t_name})
+        const list = await queries.getList()
+        res.json(list)
+    }
 })
 module.exports = router;
